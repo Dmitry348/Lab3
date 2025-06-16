5a358a4f219a0f226059a3b79af971f96b65d367
Dmitry Polubabkin <polubabkin-da@mail.ru>
2025-06-17 00:53:13 +0300

    Fix(autotranslator): use list initial_state for GRU and extract state[0] (Keras 3 compatibility)

--- DeepLearning-main/autotranslator_pipeline.py
+++ DeepLearning-main/autotranslator_pipeline.py
@@ -111,7 +111,8 @@ class Encoder(tf.keras.Model):
         
     def call(self, x, hidden):
         x = self.embedding(x)
-        output, state = self.gru(x, initial_state=hidden)
+        output, state_list = self.gru(x, initial_state=[hidden])
+        state = state_list[0] if isinstance(state_list, (list, tuple)) else state_list
         return output, state
     
     def initialize_hidden_state(self):
@@ -150,7 +151,8 @@ class Decoder(tf.keras.Model):
         x = self.embedding(x)
         x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
         
-        output, state = self.gru(x, initial_state=hidden)
+        output, state_list = self.gru(x, initial_state=[hidden])
+        state = state_list[0] if isinstance(state_list, (list, tuple)) else state_list
         output = tf.reshape(output, (-1, output.shape[2]))
         
         x = self.fc(output)
